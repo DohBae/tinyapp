ec0184e4cbe383a4cdc2ec82dac47e4d953be309
DohBae <bdoherty92@msn.com>
2022-08-09 18:59:33 +0000

    Fix errors to allow short URLs to redirect to proper web page. Add code to allow delete buttons to remove URLs

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; 
+const PORT = 8080;
 
 app.set("view engine", "ejs");
 
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 function generateRandomStrings() {}
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca", 
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
 };
 
@@ -25,32 +25,34 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
+// passes URL data to template
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-res.render("urls_new");
+  res.render("urls_new");
 });
-
+// displays a single URL and it's shortened form
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase};
+  const {id} = req.params;
+  const longURL = urlDatabase[id];
+  const templateVars = { id, longURL };
   res.render("urls_show", templateVars);
 });
 
 // saves id and longURL key value pair to urlDatabase
 app.post("/urls", (req, res) => {
-  const id = generateRandomStrings()
-  const longURL = req.body.longURL
-  urlDatabase.id = longURL
+  const id = generateRandomStrings();
+  const longURL = req.body.longURL;
+  urlDatabase.id = longURL;
   res.redirect("/urls/:id");
 });
 
-// Allow us to see the new form in the browser
+// Allow us to see the short URL and long URL in the browser
 app.post("/urls", (req, res) => {
   console.log(req.body); //Log the POST request body to the console
   res.send("Ok"); //Respond with "OK" (will be replaced)
@@ -59,12 +61,22 @@ app.post("/urls", (req, res) => {
 // redirects short URL to the long URL website
 app.get("/u/:id", (req, res) => {
   const shortURL = req.params.id;
-  console.log(urlDatabase[shortURL])
+  console.log(urlDatabase[shortURL]);
   res.redirect(urlDatabase[shortURL]);
-  
-})
+});
+
+// remove the url when delete button is pressed
+app.post("/urls/:id/delete", (req, res) => {
+  const { id } = req.params;
+  for (let shortURL in urlDatabase) {
+    if (shortURL === id) {
+      delete urlDatabase[shortURL];
+      res.redirect("/urls");
+    }
+  }
+});
 ///////////////////////////////////////////////////////////////////////////////////////
-// 
+//
 ///////////////////////////////////////////////////////////////////////////////////////
 
 app.listen(PORT, () => {
